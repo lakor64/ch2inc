7acc87d6b4ef21f6b602305c3246adcf236a7527
Christian Rendina <pizzaiolo100@proton.me>
2024-05-10 21:41:21 +0200

    Add test readme and fix bat file again

--- test/run_ch2inc.bat
+++ test/run_ch2inc.bat
@@ -2,8 +2,6 @@
 
 mkdir ch2inc 2>NUL
 
-set ch2inc_path=..\x64\Debug\
-
 for %%i in (.\*.h) do (
 	echo Parse %%i
     call :h2incfnc %%i
@@ -12,7 +10,7 @@ for %%i in (.\*.h) do (
 goto quit
 
 :h2incfnc
-"%ch2inc_path%ch2inc.exe" --msvc --nologo -d "%ch2inc_path%\ch2drvmasm.dll" -p win -b 32 "%1" "ch2inc\%1.inc"
+"ch2inc.exe" --msvc --nologo -d "ch2drvmasm.dll" -p win -b 32 "%1" "ch2inc\%1.inc"
 exit /b 0
 
 :quit
