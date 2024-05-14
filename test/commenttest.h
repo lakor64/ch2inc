41a472f5367ce30c248985d0f70d05797deadc04
Christian Rendina <pizzaiolo100@proton.me>
2024-05-15 00:26:07 +0200

    Remove entry on bugs as preserving comments is not part of h2inc job

---
+++ test/commenttest.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+// this a function
+void a();
+
+/* this is another function
+*/
+void b();
+
+/** this is a more documented function
+*/
+void c();
+
+/// no way another function mindblown
+void d();
